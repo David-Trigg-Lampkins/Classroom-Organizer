7335c0cace66c112167092300d991e0d4110dcff
WeLuvCameron-G59 <cmbailey42@tntech.edu>
2024-10-26 14:20:33 -0500

    Linked List (Most of it)
    I have about all of the voids for the linked list, going to do the extras tomorrow

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -3,3 +3,155 @@
 
 using namespace std;
 
+template <class T>
+struct Node {
+    int data;
+    Node* next;
+};
+
+template <class T>
+class LinkedList {
+
+    Node* head;
+
+public:
+
+    LinkedList() : head(NULL) {}
+
+    void insertAtBeginning(int value) {
+        Node* newNode = new Node(); 
+        newNode->data = value;      
+        newNode->next = head;      
+        head = newNode;
+    }
+
+    void insertAtPosition(int value, int position) {
+        if (position < 1) {
+            cout << "Position should be >= 1." << endl;
+            return;
+        }
+
+        if (position == 1) {
+            insertAtBeginning(value);
+            return;
+        }
+
+        Node* newNode = new Node(); 
+        newNode->data = value;     
+
+        
+        Node* temp = head;
+        for (int i = 1; i < position - 1 && temp; ++i) {
+            temp = temp->next;
+        }
+
+        
+        if (!temp) {
+            cout << "The position is out of range." << endl;
+            delete newNode;
+            return;
+        }
+
+        
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+
+    void insertAtEnd(int value) {
+        Node* newNode = new Node(); 
+        newNode->data = value;      
+        newNode->next = NULL;       
+
+        // If the list is empty, update the head to the new node
+        if (!head) {
+            head = newNode;
+            return;
+        }
+
+        // Moves to the last node
+        Node* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+
+        // Update the last node's next to the new node
+        temp->next = newNode;
+    }
+
+    void deleteFromBeginning() {
+        if (!head) {
+            cout << "List is empty." << endl;
+            return;
+        }
+
+        Node* temp = head; 
+        head = head->next; 
+        delete temp;      
+    }
+
+    void deleteFromPosition(int position) {
+        if (position < 1) {
+            cout << "Position should be >= 1." << endl;
+            return;
+        }
+        //If there is only one, it deletes that
+        if (position == 1) { 
+            deleteFromBeginning();
+            return;
+        }
+
+        Node* temp = head;
+        for (int i = 1; i < position - 1 && temp; ++i) {
+            temp = temp->next;
+        }
+
+        if (!temp || !temp->next) {
+            cout << "The position is out of range." << endl;
+            return;
+        }
+        // Save the node to be deleted
+        Node* nodeToDelete = temp->next; 
+        // Update the next pointer
+        temp->next = temp->next->next;   
+         // Delete the node
+        delete nodeToDelete;            
+    }
+
+    void deleteFromEnd() {
+        if (!head) {
+            cout << "List is empty." << endl;
+            return;
+        }
+
+        if (!head->next) {
+            delete head;   
+            head = NULL;   
+            return;
+        }
+
+        // Traverse to the second-to-last node
+        Node* temp = head;
+        while (temp->next->next) {
+            temp = temp->next;
+        }
+        
+        //  Delete the last node
+        delete temp->next; 
+        // Set the second-to-last node's next to NULL
+        temp->next = NULL; 
+    }
+
+    void print() {
+        if (!head) {
+            cout << "List is empty." << endl;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp) {
+            cout << temp->data << " -> "; 
+            temp = temp->next;
+        }
+        cout << "NULL" << endl; 
+    }
+};
