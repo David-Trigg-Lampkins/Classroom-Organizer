4273621e9392f0135bc10406d939579b0f02cfdd
WeLuvCameron-G59 <cmbailey42@tntech.edu>
2024-11-01 12:01:58 -0500

    Linked List Update
    Finished list

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -90,15 +90,74 @@ void List<T>::print() const {
 
 template <class T>
 bool List<T>::isEmpty() {
-
+    if(!head)
+		return true;
+	else
+		return false;
 }
 
 template <class T>
 int List<T>::getLength() {
-
+    int counter = 0;
+	ListNode *nodePtr;
+	
+	nodePtr = head;
+	
+	while(nodePtr != tail)
+	{
+		counter++;
+		nodePtr = nodePtr->next;
+		if (nodePtr == tail)
+			counter++;
+	}
+	return counter;
 }
 
 template <class T>
 void List<T>::sort() const {
+    
+    node* MergeSort(node *my_node)
+{
+    node *secondNode;
 
+    if (my_node == NULL)
+        return NULL;
+    else if (my_node->next == NULL)
+        return my_node;
+    else
+    {
+        secondNode = Split(my_node);
+        return Merge(MergeSort(my_node),MergeSort(secondNode));
+    }
+}
+
+node* Merge(node* firstNode, node* secondNode)
+{
+    if (firstNode == NULL) return secondNode;
+    else if (secondNode == NULL) return firstNode;
+    else if (firstNode->number <= secondNode->number) //if I reverse the sign to >=, the behavior reverses
+    {
+        firstNode->next = Merge(firstNode->next, secondNode);
+        return firstNode;
+    }
+    else 
+    {
+        secondNode->next = Merge(firstNode, secondNode->next);
+        return secondNode;
+    }
+}
+
+node* Split(node* my_node)
+{
+    node* secondNode;
+
+    if (my_node == NULL) return NULL;
+    else if (my_node->next == NULL) return NULL;
+    else {
+        secondNode = my_node->next;
+        my_node->next = secondNode->next;
+        secondNode->next = Split(secondNode->next);
+        return secondNode;
+    }
+}
 }
